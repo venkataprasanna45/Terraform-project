e7cf891ab8c510c4d4b5e411ba754ce5686ccb97
EC2 Default User <ec2-user@ip-172-31-60-152.ec2.internal>
2023-02-07 05:25:03 +0000

    first

---
+++ provider.tf
@@ -0,0 +1,8 @@
+#Update provider
+#AWS as our provider
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIARWNEGFWXZJZZ7BMP"
+  secret_key = "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"
+}
---
+++ rds.tf
@@ -0,0 +1,20 @@
+resource "aws_db_subnet_group" "default" {
+ name = "tera"
+ subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+tags = {
+ Name = "My DB subnet group"
+}
+}
+resource "aws_db_instance" "default" {
+ allocated_storage = 10
+ db_subnet_group_name = aws_db_subnet_group.default.id
+ engine = "mysql"
+ engine_version = "5.7"
+ instance_class = "db.t2.micro"
+ multi_az = true
+ db_name = "mydb"
+ username = "username"
+ password = "password"
+ skip_final_snapshot = true
+ vpc_security_group_ids = [aws_security_group.database-sg.id]
+}
