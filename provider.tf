4d28e321e72bbf4cf27645f117f2302b14e43462
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2023-02-07 11:47:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARWNEGFWXZJZZ7BMP"
-  secret_key = "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"
+  access_key = "Abc"
+  secret_key = "Ocb"
 }
