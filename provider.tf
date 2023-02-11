58812efe08f55728360079e514d4174a458d6d6d
venkataprasanna45 <112848899+venkataprasanna45@users.noreply.github.com>
2023-02-11 12:25:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARWNEGFWXXCD652PO"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
